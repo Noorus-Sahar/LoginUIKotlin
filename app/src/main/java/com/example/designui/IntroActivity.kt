00d6e663e016e1f00a0b073a7211e4b837ec9c86
Noorus-Sahar <noorussahar27@gmail.com>
2023-03-18 20:38:42 +0500

    first commit

---
+++ app/src/main/java/com/example/designui/IntroActivity.kt
@@ -0,0 +1,25 @@
+package com.example.designui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro2)
+    }
+
+    fun onClick(view: View?){
+        if (view is Button)
+        {
+            val intent = Intent(this,LoginActivity::class.java);
+            startActivity(intent);
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/designui/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.designui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+    }
+    fun onClick(view: View?){
+        if (view is Button)
+        {
+            val intent = Intent(this,SignUpActivity::class.java);
+            startActivity(intent);
+        }
+
+    }
+}
